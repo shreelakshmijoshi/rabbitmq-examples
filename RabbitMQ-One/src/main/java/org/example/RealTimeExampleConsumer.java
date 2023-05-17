124c58c930cfc7c79c291f4abf20ed4921005bf8
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2023-05-17 09:42:44 +0530

    Create RealTimeConsumer

--- RabbitMQ-One/src/main/java/org/example/RealTimeExampleConsumer.java
+++ RabbitMQ-One/src/main/java/org/example/RealTimeExampleConsumer.java
@@ -3,28 +3,26 @@ package org.example;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
 import org.json.JSONObject;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.TimeoutException;
 
-public class RealTimeExample {
-    public static final String QUEUE_NAME = "Queue-for-json-message";
-
+public class RealTimeExampleConsumer {
     public static void main(String[] args) throws IOException, TimeoutException {
         ConnectionFactory connectionFactory = new ConnectionFactory();
         Connection connection = connectionFactory.newConnection();
         Channel channel = connection.createChannel();
 
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("from_date", "01-Jan-2019")
-                .put("to_date", "09-Jan-2019")
-                .put("email","user@gmail.com")
-                .put("query", "SELECT * FROM DB_TABLE");
+        DeliverCallback callback = ((consumerTag, body) -> {
+            String message = new String(body.getBody());
+            JSONObject jsonObject1 = new JSONObject(message);
+            System.out.println("Received this JSON Object : " + jsonObject1);
+        });
 
-        channel.basicPublish("", QUEUE_NAME,null, jsonObject.toString().getBytes(StandardCharsets.UTF_8));
-        channel.close();
-        connection.close();
+        channel.basicConsume(RealTimeExample.QUEUE_NAME, true, callback, consumerTag -> {
+        });
     }
 }
