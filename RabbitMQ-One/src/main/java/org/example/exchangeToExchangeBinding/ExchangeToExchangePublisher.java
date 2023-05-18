f6f7e7b9804a5cc4637c18c754fbe26f13ac8c8d
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2023-05-18 11:37:07 +0530

    Create Exchange to exchange publishing

---
+++ RabbitMQ-One/src/main/java/org/example/exchangeToExchangeBinding/ExchangeToExchangePublisher.java
@@ -0,0 +1,33 @@
+package org.example.exchangeToExchangeBinding;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Logger;
+
+public class ExchangeToExchangePublisher {
+
+    private static final Logger LOGGER = Logger.getLogger("DirectPublisher.class");
+    public static final String DEFAULT_QUEUE = "mobile";
+    private static final String ROUTING_KEY = "fanout";
+    private static final String MESSAGE = "This is exchange to exchange";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("localhost");
+        try(Connection connection = connectionFactory.newConnection();
+            Channel channel = connection.createChannel()){
+
+                channel.basicPublish("direct-exchange", ROUTING_KEY, null, MESSAGE.getBytes(StandardCharsets.UTF_8));
+                LOGGER.info("Sent message : " + MESSAGE);
+        }
+
+//        channel.close();
+//        connection.close();
+    }
+
+}
